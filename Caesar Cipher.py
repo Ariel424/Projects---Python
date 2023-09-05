a262bed6289d32624892cc55456d6749e5081da6
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2023-09-05 16:54:57 +0300

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,3 +1,5 @@
+# This Python code defines a simple Caesar cipher program. Caesar cipher is a basic encryption technique that shifts each letter in a text by a fixed number of positions down or up the alphabet.
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
